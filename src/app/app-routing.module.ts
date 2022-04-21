9deffa444b266011f35da48174b03fd25da05c24
emre kucuk <emre.kucuk@logo.com.tr>
2022-04-22 02:33:15 +0300

    22 nisan ilk proje gönderme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth-guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "login", component: LoginComponent },
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "register", component: RegisterComponent },
+  { path: "profile", component: ProfileComponent,canActivate: [AuthGuard] },
+  { path: "product-detail", component: ProductDetailComponent },
+  { path: "**", redirectTo: "", pathMatch: "full" },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './services/alertify.service';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './auth/auth-guard';
+import { NavBarComponent } from './topbar/nav-bar/nav-bar.component';
+import { CityService } from './services/city.service';
+import { FormatCityPipe } from './pipes/format-city.pipe';
+import { ProfileComponent } from './profile/profile.component';
+import { ApiService } from './services/api.service';
+
+import { environment } from 'src/environments/environment';
+import { ProductService } from './services/product.service';
+import { CurrencyPipe } from '@angular/common';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavBarComponent,
+    FormatCityPipe,
+    ProfileComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule, ReactiveFormsModule,
+
+
   ],
-  providers: [],
+  providers: [AlertifyService, AuthGuard, AuthService, CityService, ApiService, ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
